0138c266ada8e60aa22f78e67d43cc34abf1711a
Kemal YILDIRIM <kemal_yldrim@outlook.com>
2018-11-25 18:09:17 +0300

    head and body components added

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Image} from 'react-native';
+
+class Body extends Component() {
+    render() {
+        return (
+            <View>
+                <Image></Image>
+            </View>
+        );
+    }
+}
+
+export default Body;
