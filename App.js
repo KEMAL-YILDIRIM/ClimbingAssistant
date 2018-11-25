f634ee76cb8d5a98bc12b1cc8c4124edd7a9f183
Kemal YILDIRIM <kemal_yldrim@outlook.com>
2018-11-25 20:13:50 +0300

    app js reformatted

--- App.js
+++ App.js
@@ -7,16 +7,16 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import Head from './src/components/head';
-import Body from './src/components/body';
+import {StyleSheet, View} from 'react-native';
+import Head from './src/components/Head.js';
+import Body from './src/components/Body.js';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <Head></Head>
-      <Body></Body>
+        <Head></Head>
+        <Body></Body>
       </View>
     );
   }
@@ -26,6 +26,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
-    marginTop: 10,
+    marginTop: 10
   }
 });
